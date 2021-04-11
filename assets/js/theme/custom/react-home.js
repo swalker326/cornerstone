e3ed051cdce984c2f3e27ca4f47161bdc93dadd9
Shane <shane@steveross.io>
2021-04-11 00:03:34 -0500

    Add home page react component

---
+++ assets/js/theme/custom/react-home.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
+import PageManager from '../page-manager';
+import Breadcrumbs from '../react/Breadcrumbs';
+
+const Home = (props) => {
+  console.log("home props", props); // eslint-disable-line
+return(
+   <div className='react-home' >
+   <Breadcrumbs breadcrumbs={props.context.breadcrumbs} />
+    HOME
+   </div>
+ )
+}
+
+export default class ReactHome extends PageManager {
+  onReady() {
+      const container = $('#root')[0];
+      ReactDOM.render(<Home context={this.context}/>, container);
+  }
+}
